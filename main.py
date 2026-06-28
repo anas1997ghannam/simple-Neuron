fd1a7a03ce6ed162392e4f2b68812f8118195e85
anasGh <anasghannam9797@gmail.com>
2026-06-28 17:04:21 +0300

    Implement First Neural Network Layer

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import numpy as np
 
-inputs = np.array([[1,2,3]])
-weights = np.array([[0.2,0.8,-0.5]])
-bias = 2
+inputs = np.array([1, 2, 3])
 
-output = np.dot(inputs,weights.T)+bias
-print(output)
+weights = np.array([
+    [0.2, 0.8, -0.5],
+    [0.5, -0.91, 0.26],
+    [-0.26, -0.27, 0.17]
+])
+
+biases = np.array([2, 3, 0.5])
+
+outputs = np.dot(weights, inputs) + biases
+
+print(outputs)
+print(np.dot(weights[0], inputs)+biases[0])
+print(np.dot(weights[1], inputs)+biases[1])
+print(np.dot(weights[2], inputs)+biases[2])
