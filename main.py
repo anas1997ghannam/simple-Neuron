0dac726fd89af9815feb5a9c8e7aed15d1d2fc43
anasGh <anasghannam9797@gmail.com>
2026-06-28 17:39:33 +0300

    Implement forward pass with batches

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
+#simple neural network with 3 inputs, 1 neuron, and 1 output
+# import numpy as np
+
+# inputs = np.array([1, 2, 3])
+# weights = np.array([0.2, 0.8, -0.5])
+# bias = 2
+
+# output = np.dot(inputs, weights) + bias
+
+# print(output)
+
+#simple neural network with 3 inputs, 3 neurons, and 1 output
+# import numpy as np
+
+# inputs = np.array([1, 2, 3])
+
+# weights = np.array([
+#     [0.2, 0.8, -0.5],
+#     [0.5, -0.91, 0.26],
+#     [-0.26, -0.27, 0.17]
+# ])
+
+# biases = np.array([2, 3, 0.5])
+
+# outputs = np.dot(weights, inputs) + biases
+
+# print(outputs)
+# print(np.dot(weights[0], inputs)+biases[0])
+# print(np.dot(weights[1], inputs)+biases[1])
+# print(np.dot(weights[2], inputs)+biases[2])
+
 import numpy as np
 
-inputs = np.array([1, 2, 3])
+inputs = np.array([
+    [1,2,3],
+    [2,5,1],
+    [4,2,1]
+])
 
 weights = np.array([
     [0.2, 0.8, -0.5],
@@ -10,9 +45,10 @@ weights = np.array([
 
 biases = np.array([2, 3, 0.5])
 
-outputs = np.dot(weights, inputs) + biases
-
+outputs = np.dot(inputs, weights.T) + biases
 print(outputs)
-print(np.dot(weights[0], inputs)+biases[0])
-print(np.dot(weights[1], inputs)+biases[1])
-print(np.dot(weights[2], inputs)+biases[2])
+
+print(inputs.shape)
+print(weights.shape)
+print(weights.T.shape)
+print(outputs.shape)
